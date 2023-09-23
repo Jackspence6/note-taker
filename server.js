76a6976366139d66ec598539b88db9aa3b972fb0
Jackspence6 <jackspence123456@gmail.com>
2023-09-23 15:23:06 +0200

    Added a delete Route for /api/note/:id.

--- server.js
+++ server.js
@@ -45,6 +45,24 @@ app.get("/notes", (req, res) => {
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+// Delete Route for /api/note/:id
+app.delete("/api/note/:id", (req, res) => {
+	// Read the existing notes from db.json file
+	let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
+
+	// Finding the correct note to delete
+	const noteId = req.params.id;
+
+	// Filtering out the note with the given id
+	const filteredNotes = notes.filter((note) => note.id !== noteId);
+
+	// Write the filtered notes back to the db.json file
+	fs.writeFileSync("./db/db.json", JSON.stringify(filteredNotes), "utf-8");
+
+	// Send back a success response
+	res.status(200).send("Note deleted successfully");
+});
 /******************************************/
 /* Database Connections */
 /******************************************/
